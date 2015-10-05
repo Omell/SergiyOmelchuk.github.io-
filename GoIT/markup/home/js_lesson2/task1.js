572c39184faaa155c1841894dd9485f8bf5b8ef1
SergiyOmelchuk <om.sergiy@gmail.com>
2015-10-05 16:30:45 +0200

    js
    js

---
+++ GoIT/markup/home/js_lesson2/task1.js
@@ -0,0 +1,11 @@
+/**
+ * Created by sergey on 05.10.2015.
+ */
+var year = +prompt('Какой сегодня год');
+
+if (year == 2015) {
+    alert('Вы правы!')
+}
+else {
+    alert('С луны свалися? 2015!')
+}
