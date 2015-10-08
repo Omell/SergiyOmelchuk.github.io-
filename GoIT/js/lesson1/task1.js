c3fa2b36a20ea5aee106333c4a1d4eaba3967022
SergiyOmelchuk <om.sergiy@gmail.com>
2015-10-08 17:43:32 +0200

    js
    js

---
+++ GoIT/js/lesson1/task1.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sergey on 08.10.2015.
+ */
+
+var developer;
+var name = 'Sergey';
+
+developer = name;
+
+alert (developer);
---
+++ GoIT/js/lesson1/task2.js
@@ -0,0 +1,5 @@
+/**
+ * Created by sergey on 08.10.2015.
+ */
+var name = prompt('Введите Ваше имя');
+alert (name);
