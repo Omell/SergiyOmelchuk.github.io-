486405469c48db437cf5317ff289ae2f7d522054
SergiyOmelchuk <om.sergiy@gmail.com>
2015-12-27 13:15:47 +0200

    2712
    2712

---
+++ GoIT/js/exam/sfx/js.js
@@ -0,0 +1,6 @@
+/**
+ * Created by sergey on 25.12.2015.
+ */
+function gunmen () {
+    transition(gunmen.left=50%, 5);
+}
---
+++ GoIT/js/lesson16/task2.js
@@ -0,0 +1,6 @@
+document.getElementById('age-table').querySelectorAll('label').length;
+document.getElementById('age-table').getElementsByTagName('td')[0];
+document.getElementsByTagName('form')[1];
+document.querySelector('form[name="search"]');
+document.getElementsByName('info[0]');
+document.querySelector('form[name="search-person"] [name="info[0]"]');
