bde9054aee2952a491e5a3e7c6aa630d06404738
SergiyOmelchuk <om.sergiy@gmail.com>
2016-01-21 11:29:16 +0200

    1
    1

---
+++ Practice/shpp.com.ua/js/script.js
@@ -0,0 +1,12 @@
+
+ 
+ ;(function() {
+ 
+$('a[href^="#"]').click(function(){
+        var el = $(this).attr('href');
+        $('body').animate({
+            scrollTop: $(el).offset().top}, 2000);
+        return false; 
+});
+    
+});
