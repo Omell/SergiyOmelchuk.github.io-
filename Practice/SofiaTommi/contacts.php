4d4eff5e5c1adf3a76c00147af0d9acdee846724
SergiyOmelchuk <om.sergiy@gmail.com>
2016-05-11 01:39:56 +0200

    6

--- Practice/SofiaTommi/contacts.php
+++ Practice/SofiaTommi/contacts.php
@@ -1,14 +1,13 @@
 <?php
 if (isset ($_POST['contactFF'])) {
-  $to = "om.sergiy@gmail.com"; // поменять на свой электронный адрес
+  $to = "name@yandex.ru"; // поменять на свой электронный адрес
   $from = $_POST['contactFF'];
   $subject = "Заполнена контактная форма с ".$_SERVER['HTTP_REFERER'];
   $message = "Имя: ".$_POST['nameFF']."\nEmail: ".$from."\nIP: ".$_SERVER['REMOTE_ADDR']."\nСообщение: ".$_POST['messageFF'];
   $boundary = md5(date('r', time()));
   $filesize = '';
   $headers = "MIME-Version: 1.0\r\n";
-  $headers .= "From: " . $from . "\r\n";
-  $headers .= "Reply-To: " . $from . "\r\n";
+
   $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
   $message="
 Content-Type: multipart/mixed; boundary=\"$boundary\"
