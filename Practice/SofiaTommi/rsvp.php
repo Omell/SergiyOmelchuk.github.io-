5781d82519ff9691724ce83f3581ae6a3b2aab96
SergiyOmelchuk <om.sergiy@gmail.com>
2016-05-11 11:09:42 +0200

    7

--- Practice/SofiaTommi/rsvp.php
+++ Practice/SofiaTommi/rsvp.php
@@ -20,7 +20,7 @@
 			}
 		}
 	
-		$email_to  =  'some@email.com'; 
+		$email_to  =  'omelseo@gmail.com';
 		
 		$headers = "From: ".$_POST["inputemail"]."\r\n";	
 		$headers .= "Reply-To: ".$_POST["inputemail"]."\r\n";	
