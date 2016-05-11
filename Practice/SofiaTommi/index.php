91f40c946b1a391c53c762b174dc4a53345fd97d
SergiyOmelchuk <om.sergiy@gmail.com>
2016-05-11 11:38:21 +0200

    477

---
+++ Practice/SofiaTommi/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+
+
+<?php
+$message="hello";
+$to = "omelseo@gmail.com";
+$subject = "tema";
+mail ($to, $subject, $message);
+?>
+
+</body>
+</html>
